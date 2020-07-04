0a0896dbca9d3a1cc0cd064aceb2d7f0e52756f3
Vinicius Herculano <viniciusherculanomap@gmail.com>
2020-07-04 17:57:24 -0300

    Primeiro controller e testes na página Razor

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Projeto MVC curso C#";
 
             return View();
         }
